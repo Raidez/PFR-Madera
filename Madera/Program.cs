9024cfc80adb56400737b60c23ec992a91205f78
Alexis LUCIEN <alexis.lucien@outlook.fr>
2018-11-25 16:16:48 +0100

    First commit

---
+++ Madera/Program.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Crée par SharpDevelop.
+ * Utilisateur: alexis
+ * Date: 25/11/2018
+ * Heure: 16:15
+ * 
+ * Pour changer ce modèle utiliser Outils | Options | Codage | Editer les en-têtes standards.
+ */
+using System;
+using System.Windows.Forms;
+
+namespace Madera
+{
+	/// <summary>
+	/// Class with program entry point.
+	/// </summary>
+	internal sealed class Program
+	{
+		/// <summary>
+		/// Program entry point.
+		/// </summary>
+		[STAThread]
+		private static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Application.Run(new MainForm());
+		}
+		
+	}
+}
