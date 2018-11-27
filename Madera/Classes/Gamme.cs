5c8ae1f71ac88e721d65f8860296d0804a806f1b
ObscureAngel <Obscure-Angel@gmx.fr>
2018-11-27 13:09:55 +0100

    Ajout des définition des classe Gamme, Parametre, Fournisseur, Matière et Module.

---
+++ Madera/Classes/Gamme.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Madera.Classes
+{
+	class Gamme
+	{
+		public int gamId { get; set; }
+		public string gamLibelle { get; set; }
+
+		public Gamme(int gamId, string gamLibelle)
+		{
+			this.gamId = gamId;
+			this.gamLibelle = gamLibelle;
+		}
+	}
+}
---
+++ Madera/Classes/Module.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Madera.Classes
+{
+	class Module
+	{
+		public int modID { get; set; }
+		public string modLibele { get; set; }
+		public Gamme modGamme { get; set; }
+		public Matiere modMatiere { get; set; }
+		public double prixBase { get; set; }
+		public List<Parametre> modParametres { get; set; }
+
+		public Module(int modID, string modLibele, Gamme modGamme, Matiere modMatiere, double prixBase, List<Parametre> modParametres)
+		{
+			this.modID = modID;
+			this.modLibele = modLibele;
+			this.modGamme = modGamme;
+			this.modMatiere = modMatiere;
+			this.prixBase = prixBase;
+			this.modParametres = modParametres;
+		}
+	}
+}
