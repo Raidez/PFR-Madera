aab6af73a4672c74bbedcb73e0ca06b18a7abca9
ObscureAngel <Obscure-Angel@gmx.fr>
2018-11-28 12:43:47 +0100

    Création des classe devis, client, salarié. Ajout d'une liste statique dans la gamme

--- Madera/Classes/Gamme.cs
+++ Madera/Classes/Gamme.cs
@@ -10,11 +10,26 @@ namespace Madera.Classes
 	{
 		public int gamId { get; set; }
 		public string gamLibelle { get; set; }
+		public static List<Gamme> listGamme { get; set; }
 
 		public Gamme(int gamId, string gamLibelle)
 		{
 			this.gamId = gamId;
 			this.gamLibelle = gamLibelle;
+		}
+
+		public void ajoutGamme(Gamme g) {
+			listGamme.Add(g);
+		}
+
+		public void modifierGamme(Gamme g) {
+
+		}
+
+		public bool supprimeGamme(Gamme g) {
+			
+
+			return true;
 		}
 	}
 }
