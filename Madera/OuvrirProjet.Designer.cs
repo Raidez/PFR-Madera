52cfd758dec888f59a71a1fd610e5749b85d112d
briffaut <loic.douchet41@gmail.com>
2018-11-30 10:59:53 +0100

    Ouvrir projet En cours
    PAS FINIS OK ?!?

---
+++ Madera/OuvrirProjet.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace Madera
+{
+    partial class OuvrirProjet
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnRetour = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.BtnDeconnection = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.FlowLayoutPanelListeProjet = new System.Windows.Forms.FlowLayoutPanel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.FlowLayoutPanelListeProjet.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // BtnRetour
+            // 
+            this.BtnRetour.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnRetour.Location = new System.Drawing.Point(895, 631);
+            this.BtnRetour.Name = "BtnRetour";
+            this.BtnRetour.Size = new System.Drawing.Size(164, 77);
+            this.BtnRetour.TabIndex = 31;
+            this.BtnRetour.Text = "Retour";
+            this.BtnRetour.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.FlowLayoutPanelListeProjet);
+            this.panel1.Location = new System.Drawing.Point(21, 138);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1038, 487);
+            this.panel1.TabIndex = 28;
+            // 
+            // BtnDeconnection
+            // 
+            this.BtnDeconnection.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnDeconnection.Location = new System.Drawing.Point(21, 631);
+            this.BtnDeconnection.Name = "BtnDeconnection";
+            this.BtnDeconnection.Size = new System.Drawing.Size(164, 77);
+            this.BtnDeconnection.TabIndex = 30;
+            this.BtnDeconnection.Text = "déconnection";
+            this.BtnDeconnection.UseVisualStyleBackColor = true;
+            // 
+            // panel4
+            // 
+            this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel4.Controls.Add(this.label5);
+            this.panel4.Controls.Add(this.label4);
+            this.panel4.Location = new System.Drawing.Point(191, 631);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(698, 77);
+            this.panel4.TabIndex = 29;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(14, 46);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(121, 17);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Date de création :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 17);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Nom du client :";
+            // 
+            // FlowLayoutPanelListeProjet
+            // 
+            this.FlowLayoutPanelListeProjet.Controls.Add(this.panel2);
+            this.FlowLayoutPanelListeProjet.Location = new System.Drawing.Point(3, 3);
+            this.FlowLayoutPanelListeProjet.Name = "FlowLayoutPanelListeProjet";
+            this.FlowLayoutPanelListeProjet.Padding = new System.Windows.Forms.Padding(40, 0, 0, 0);
+            this.FlowLayoutPanelListeProjet.Size = new System.Drawing.Size(1030, 479);
+            this.FlowLayoutPanelListeProjet.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::Madera.Properties.Resources.dossier;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(0, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 82);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::Madera.Properties.Resources.logoMadera;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(21, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(406, 120);
+            this.pictureBox1.TabIndex = 27;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Location = new System.Drawing.Point(43, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(106, 125);
+            this.panel2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Roboto", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(13, 88);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 25);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Projet 1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // OuvrirProjet
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1080, 720);
+            this.Controls.Add(this.BtnRetour);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.BtnDeconnection);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel4);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "OuvrirProjet";
+            this.Text = "OuvrirProjet";
+            this.panel1.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.FlowLayoutPanelListeProjet.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnRetour;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.FlowLayoutPanel FlowLayoutPanelListeProjet;
+        private System.Windows.Forms.Button BtnDeconnection;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label1;
+    }
+}
--- Madera/Properties/Resources.Designer.cs
+++ Madera/Properties/Resources.Designer.cs
@@ -73,6 +73,16 @@ namespace Madera.Properties {
         /// <summary>
         ///   Recherche une ressource localisée de type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap dossier {
+            get {
+                object obj = ResourceManager.GetObject("dossier", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Recherche une ressource localisée de type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap logoMadera {
             get {
                 object obj = ResourceManager.GetObject("logoMadera", resourceCulture);
