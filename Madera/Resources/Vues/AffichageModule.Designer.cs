308574bebbcd4041c1646c6c8636d5d479dd594e
briffaut <loic.douchet41@gmail.com>
2018-11-30 14:02:16 +0100

    maj des vues global

---
+++ Madera/Resources/Vues/AffichageModule.Designer.cs
@@ -0,0 +1,251 @@
+﻿namespace Madera
+{
+    partial class AffichageModule
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.BtnModifierEtat = new System.Windows.Forms.Button();
+            this.LblEtatDevis = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.flowLayoutPanelListModule = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panelNavBar = new System.Windows.Forms.Panel();
+            this.BtnReduce = new System.Windows.Forms.Button();
+            this.BtnExit = new System.Windows.Forms.Button();
+            this.BtnRetour = new System.Windows.Forms.Button();
+            this.BtnDeconnection = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.BtnModifierEtat);
+            this.panel1.Controls.Add(this.LblEtatDevis);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.flowLayoutPanelListModule);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(16, 195);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1038, 430);
+            this.panel1.TabIndex = 29;
+            // 
+            // BtnModifierEtat
+            // 
+            this.BtnModifierEtat.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnModifierEtat.Location = new System.Drawing.Point(920, 348);
+            this.BtnModifierEtat.Name = "BtnModifierEtat";
+            this.BtnModifierEtat.Size = new System.Drawing.Size(98, 44);
+            this.BtnModifierEtat.TabIndex = 5;
+            this.BtnModifierEtat.Text = "Modifier";
+            this.BtnModifierEtat.UseVisualStyleBackColor = true;
+            // 
+            // LblEtatDevis
+            // 
+            this.LblEtatDevis.AutoSize = true;
+            this.LblEtatDevis.Location = new System.Drawing.Point(852, 362);
+            this.LblEtatDevis.Name = "LblEtatDevis";
+            this.LblEtatDevis.Size = new System.Drawing.Size(51, 17);
+            this.LblEtatDevis.TabIndex = 4;
+            this.LblEtatDevis.Text = "default";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(755, 362);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Etat du devis : ";
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(822, 288);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(196, 44);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Gestion du paiement ";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // flowLayoutPanelListModule
+            // 
+            this.flowLayoutPanelListModule.Location = new System.Drawing.Point(34, 53);
+            this.flowLayoutPanelListModule.Name = "flowLayoutPanelListModule";
+            this.flowLayoutPanelListModule.Size = new System.Drawing.Size(610, 349);
+            this.flowLayoutPanelListModule.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(31, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Liste des modules";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::Madera.Properties.Resources.logoMadera;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(16, 69);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(406, 120);
+            this.pictureBox1.TabIndex = 28;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panelNavBar
+            // 
+            this.panelNavBar.Location = new System.Drawing.Point(-1, 0);
+            this.panelNavBar.Name = "panelNavBar";
+            this.panelNavBar.Size = new System.Drawing.Size(995, 63);
+            this.panelNavBar.TabIndex = 27;
+            // 
+            // BtnReduce
+            // 
+            this.BtnReduce.FlatAppearance.BorderSize = 0;
+            this.BtnReduce.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnReduce.Location = new System.Drawing.Point(1000, 12);
+            this.BtnReduce.Name = "BtnReduce";
+            this.BtnReduce.Size = new System.Drawing.Size(35, 34);
+            this.BtnReduce.TabIndex = 26;
+            this.BtnReduce.Text = "_";
+            this.BtnReduce.UseVisualStyleBackColor = true;
+            // 
+            // BtnExit
+            // 
+            this.BtnExit.FlatAppearance.BorderSize = 0;
+            this.BtnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnExit.Location = new System.Drawing.Point(1041, 18);
+            this.BtnExit.Name = "BtnExit";
+            this.BtnExit.Size = new System.Drawing.Size(23, 23);
+            this.BtnExit.TabIndex = 25;
+            this.BtnExit.Text = "X";
+            this.BtnExit.UseVisualStyleBackColor = true;
+            // 
+            // BtnRetour
+            // 
+            this.BtnRetour.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnRetour.Location = new System.Drawing.Point(890, 631);
+            this.BtnRetour.Name = "BtnRetour";
+            this.BtnRetour.Size = new System.Drawing.Size(164, 77);
+            this.BtnRetour.TabIndex = 24;
+            this.BtnRetour.Text = "Retour";
+            this.BtnRetour.UseVisualStyleBackColor = true;
+            // 
+            // BtnDeconnection
+            // 
+            this.BtnDeconnection.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnDeconnection.Location = new System.Drawing.Point(16, 631);
+            this.BtnDeconnection.Name = "BtnDeconnection";
+            this.BtnDeconnection.Size = new System.Drawing.Size(164, 77);
+            this.BtnDeconnection.TabIndex = 23;
+            this.BtnDeconnection.Text = "déconnection";
+            this.BtnDeconnection.UseVisualStyleBackColor = true;
+            // 
+            // panel4
+            // 
+            this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel4.Controls.Add(this.label5);
+            this.panel4.Controls.Add(this.label4);
+            this.panel4.Location = new System.Drawing.Point(186, 631);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(698, 77);
+            this.panel4.TabIndex = 22;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(14, 46);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(121, 17);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Date de création :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 17);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Nom du client :";
+            // 
+            // AffichageModule
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1080, 720);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panelNavBar);
+            this.Controls.Add(this.BtnReduce);
+            this.Controls.Add(this.BtnExit);
+            this.Controls.Add(this.BtnRetour);
+            this.Controls.Add(this.BtnDeconnection);
+            this.Controls.Add(this.panel4);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "AffichageModule";
+            this.Text = "AffichageModule";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button BtnModifierEtat;
+        private System.Windows.Forms.Label LblEtatDevis;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanelListModule;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panelNavBar;
+        private System.Windows.Forms.Button BtnReduce;
+        private System.Windows.Forms.Button BtnExit;
+        private System.Windows.Forms.Button BtnRetour;
+        private System.Windows.Forms.Button BtnDeconnection;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+    }
+}
---
+++ Madera/Resources/Vues/CreerProjet.Designer.cs
@@ -0,0 +1,269 @@
+﻿namespace Madera
+{
+    partial class CreerProjet
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.BtnAjouterModule = new System.Windows.Forms.Button();
+            this.BtnValider = new System.Windows.Forms.Button();
+            this.BtnRetour = new System.Windows.Forms.Button();
+            this.BtnDeconnection = new System.Windows.Forms.Button();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.BtnAjouterModule);
+            this.panel1.Controls.Add(this.BtnValider);
+            this.panel1.Location = new System.Drawing.Point(12, 138);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1038, 487);
+            this.panel1.TabIndex = 3;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(72, 174);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(146, 17);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Ajouter un parametre ";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(190, 122);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(163, 24);
+            this.comboBox1.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(115, 122);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 17);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Gamme  :";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(190, 94);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(163, 22);
+            this.textBox2.TabIndex = 9;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(149, 17);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Prix de base ( Euros) :";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(190, 61);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(163, 22);
+            this.textBox1.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(69, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Nom du module :";
+            // 
+            // BtnAjouterModule
+            // 
+            this.BtnAjouterModule.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(204)))), ((int)(((byte)(113)))));
+            this.BtnAjouterModule.FlatAppearance.BorderSize = 0;
+            this.BtnAjouterModule.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnAjouterModule.ForeColor = System.Drawing.Color.White;
+            this.BtnAjouterModule.Location = new System.Drawing.Point(819, 453);
+            this.BtnAjouterModule.Name = "BtnAjouterModule";
+            this.BtnAjouterModule.Size = new System.Drawing.Size(121, 29);
+            this.BtnAjouterModule.TabIndex = 5;
+            this.BtnAjouterModule.Text = "Ajouter module";
+            this.BtnAjouterModule.UseVisualStyleBackColor = false;
+            // 
+            // BtnValider
+            // 
+            this.BtnValider.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(204)))), ((int)(((byte)(113)))));
+            this.BtnValider.FlatAppearance.BorderSize = 0;
+            this.BtnValider.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnValider.ForeColor = System.Drawing.Color.White;
+            this.BtnValider.Location = new System.Drawing.Point(946, 453);
+            this.BtnValider.Name = "BtnValider";
+            this.BtnValider.Size = new System.Drawing.Size(87, 29);
+            this.BtnValider.TabIndex = 4;
+            this.BtnValider.Text = "Valider";
+            this.BtnValider.UseVisualStyleBackColor = false;
+            // 
+            // BtnRetour
+            // 
+            this.BtnRetour.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnRetour.Location = new System.Drawing.Point(886, 631);
+            this.BtnRetour.Name = "BtnRetour";
+            this.BtnRetour.Size = new System.Drawing.Size(164, 77);
+            this.BtnRetour.TabIndex = 16;
+            this.BtnRetour.Text = "Retour";
+            this.BtnRetour.UseVisualStyleBackColor = true;
+            // 
+            // BtnDeconnection
+            // 
+            this.BtnDeconnection.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.BtnDeconnection.Location = new System.Drawing.Point(12, 631);
+            this.BtnDeconnection.Name = "BtnDeconnection";
+            this.BtnDeconnection.Size = new System.Drawing.Size(164, 77);
+            this.BtnDeconnection.TabIndex = 15;
+            this.BtnDeconnection.Text = "déconnection";
+            this.BtnDeconnection.UseVisualStyleBackColor = true;
+            // 
+            // panel4
+            // 
+            this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel4.Controls.Add(this.label5);
+            this.panel4.Controls.Add(this.label4);
+            this.panel4.Location = new System.Drawing.Point(182, 631);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(698, 77);
+            this.panel4.TabIndex = 14;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(14, 46);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(121, 17);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Date de création :";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 15);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 17);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Nom du client :";
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::Madera.Properties.Resources.BtnPlus;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Cursor = System.Windows.Forms.Cursors.SizeAll;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(224, 174);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(32, 25);
+            this.button1.TabIndex = 13;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = global::Madera.Properties.Resources.logoMadera;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(406, 120);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // CreerProjet
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1080, 720);
+            this.Controls.Add(this.BtnRetour);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.BtnDeconnection);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel4);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "CreerProjet";
+            this.Text = "CreerProjet";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button BtnAjouterModule;
+        private System.Windows.Forms.Button BtnValider;
+        private System.Windows.Forms.Button BtnRetour;
+        private System.Windows.Forms.Button BtnDeconnection;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+    }
+}
