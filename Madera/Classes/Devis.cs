d75625b5b3c21deb97207c32807c1de536dc59ec
ObscureAngel <Obscure-Angel@gmx.fr>
2019-03-04 17:23:49 +0100

    Définition des fonctions de la classe Devis

--- Madera/Classes/Devis.cs
+++ Madera/Classes/Devis.cs
@@ -17,6 +17,13 @@ namespace Madera.Classes
         public double devMontantFacture;
         public Client client;
         public Salarie salarie;
-        
+
+        public void ajouterModule(Module module);
+        public void retirerModule(Guid guidModule);
+        public void associerClient(Client client);
+        public void associerSalarie(Salarie salarie);
+        public void creerDevis();
+        public void signerDevis();
+        public void facturerDevis();
 	}
 }
