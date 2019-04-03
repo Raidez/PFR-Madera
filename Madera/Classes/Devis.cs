9f9d0673bba2d244817797ddd0194cc2c5ce7273
ObscureAngel <Obscure-Angel@gmx.fr>
2019-04-03 10:59:10 +0200

    Création des méthodes de la classe Devis et re-création du Resources.resx

--- Madera/Classes/Devis.cs
+++ Madera/Classes/Devis.cs
@@ -8,15 +8,51 @@ namespace Madera.Classes
 {
 	class Devis
 	{
-        public Guid devId;
-        public int devStatut;
-        public DateTime devDateCreation;
-        public bool devAccord;
-        public DateTime devDateSignature;
-        public DateTime devDateFacture;
-        public double devMontantFacture;
-        public Client client;
-        public Salarie salarie;
-        
+        public Guid devId { get; set; }
+        public int devStatut { get; set; }
+        public DateTime devDateCreation { get; set; }
+        public bool devAccord { get; set; }
+        public DateTime devDateSignature { get; set; }
+        public DateTime devDateFacture { get; set; }
+        public double devMontantFacture { get; set; }
+        public Client client { get; set; }
+        public Salarie salarie { get; set; }
+        public List<Module> listeModules { get; set; }
+
+        public void ajouterModule(Module aAjouter)
+        {
+
+        }
+
+        public void retirerModule(Module aSupprimer)
+        {
+
+        }
+
+        public void associerClient(Client aAssocier)
+        {
+
+        }
+
+        public void associerSalarie(Salarie aAssocier)
+        {
+
+        }
+
+        public void creerDevis()
+        {
+			this.devDateCreation = DateTime.Now;
+        }
+
+        public void signerDevis()
+        {
+			this.devDateSignature = DateTime.Now;
+			this.devAccord = true;
+        }
+
+        public void facturerDevis()
+        {
+			this.devDateFacture = DateTime.Now;
+        }
 	}
 }
